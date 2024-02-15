e5ff11966a69c7e7723fe61aed14045ad96c01e4
SakuniOhto <sakunilaisk@gmail.com>
2024-02-15 16:41:46 +0500

    header fix and feed text fix

--- js/index.js
+++ js/index.js
@@ -11,23 +11,49 @@ $(document).ready(async function () {
         } else {
             header.removeClass('header__active')
         }
-    }
-    )
-    $('.hygiene__images img').hover(function() {
-        $(this).animate({'border-radius': '20'})
-    }, function() {
-        $(this).animate({'border-radius': '108.5'})
-    }
-    )
+    })
+    const page = $('html, body')
+    $('header__link')
     var rotation = 0;
     var speed = 1; // начальная скорость вращения
-
+    $('.header__link').click(function () {
+        page.on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function () {
+            page.stop();
+        });
+        switch ($(this).text()) {
+            case 'Главная':
+                page.animate({ scrollTop: $(this).position().top - 50 }, 1600, function () {
+                    page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+                })
+                break;
+            case 'Питание':
+                page.animate({ scrollTop: $('.feed').position().top - 30 }, 1600, function () {
+                    page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+                })
+                break;
+            case 'Спорт':
+                page.animate({ scrollTop: $('.sport').position().top - 30 }, 1600, function () {
+                    page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+                })
+                break;
+            case 'Гигиена':
+                page.animate({ scrollTop: $('.hygiene').position().top + 80}, 1600, function () {
+                    page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+                })
+                break;
+            case 'Режим дня':
+                page.animate({ scrollTop: $('.cycle').position().top + 70 }, 1600, function () {
+                    page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+                })
+                break;
+        }
+    })
     function rotateEllipse() {
         $(".intro__path-ellipse").css({
             transform: "rotate(" + rotation + "deg)"
         });
 
-        rotation += 5 
+        rotation += 5
 
         setTimeout(rotateEllipse, 10); // повторяем вращение каждые 10 миллисекунд
     }
@@ -42,17 +68,17 @@ $(document).ready(async function () {
     $('.juice__green').animate({ top: 0 }, { duration: 1300 })
     await new Promise(r => setTimeout(r, 1300));
     function waving() {
-        $('.pears').animate({top: '-20'}, 1600)
-        $('.pears').animate({top: '20'}, 1600)
-        $('.peach').animate({top: '20'}, 1600)
-        $('.peach').animate({top: '-20'}, 1600)
-        $('.orange').animate({top: '-20'}, 1600)
-        $('.orange').animate({top: '20'}, 1600)
-        $('.strawberry').animate({top: '20'}, 1600)
-        $('.strawberry').animate({top: '-20'}, 1600)
+        $('.pears').animate({ top: '-20' }, 1600)
+        $('.pears').animate({ top: '20' }, 1600)
+        $('.peach').animate({ top: '20' }, 1600)
+        $('.peach').animate({ top: '-20' }, 1600)
+        $('.orange').animate({ top: '-20' }, 1600)
+        $('.orange').animate({ top: '20' }, 1600)
+        $('.strawberry').animate({ top: '20' }, 1600)
+        $('.strawberry').animate({ top: '-20' }, 1600)
         setTimeout(waving, 3200);
     }
     waving()
-    
+
 
 })
